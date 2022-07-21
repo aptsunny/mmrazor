6f01e7b286fc34dbff2b42564796cc8e6c68e11c
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-07-21 19:30:36 +0800

    add center crop dynamic conv2d

--- mmrazor/models/architectures/dynamic_op/__init__.py
+++ mmrazor/models/architectures/dynamic_op/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import DynamicOP
-from .default_dynamic_ops import (DynamicBatchNorm1d, DynamicBatchNorm2d,
-                                  DynamicBatchNorm3d, DynamicConv2d,
-                                  DynamicGroupNorm, DynamicInstanceNorm,
-                                  DynamicLinear, ProgressiveDynamicConv2d)
+from .default_dynamic_ops import (CenterCropDynamicConv2d, DynamicBatchNorm1d,
+                                  DynamicBatchNorm2d, DynamicBatchNorm3d,
+                                  DynamicConv2d, DynamicGroupNorm,
+                                  DynamicInstanceNorm, DynamicLinear,
+                                  ProgressiveDynamicConv2d)
 from .slimmable_dynamic_ops import SwitchableBatchNorm2d
 
 __all__ = [
