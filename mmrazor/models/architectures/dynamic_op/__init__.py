ba9099301768a00b9116575a5944724781406861
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-07-22 12:29:34 +0800

    refactor dynamic sequential

--- mmrazor/models/architectures/dynamic_op/__init__.py
+++ mmrazor/models/architectures/dynamic_op/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import DynamicOP
+from .dynamic_container import DynamicSequential
 from .dynamic_conv import (CenterCropDynamicConv2d, DynamicConv2d,
                            ProgressiveDynamicConv2d)
 from .dynamic_linear import DynamicLinear
