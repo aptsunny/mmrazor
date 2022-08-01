611a7b0a962ced600197cff87c59471514625448
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-08-01 11:10:54 +0800

    add input resizer

--- mmrazor/models/architectures/dynamic_op/__init__.py
+++ mmrazor/models/architectures/dynamic_op/__init__.py
@@ -3,6 +3,7 @@ from .base import DynamicOP
 from .dynamic_container import DynamicSequential
 from .dynamic_conv import (CenterCropDynamicConv2d, DynamicConv2d,
                            ProgressiveDynamicConv2d)
+from .dynamic_function import DynamicInputResizer
 from .dynamic_linear import DynamicLinear
 from .dynamic_norm import (DynamicBatchNorm1d, DynamicBatchNorm2d,
                            DynamicBatchNorm3d, DynamicGroupNorm,
