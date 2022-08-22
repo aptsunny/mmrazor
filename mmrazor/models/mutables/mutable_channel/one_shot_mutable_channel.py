a4b16691fa20b29cc4fc35ec49b1c16efe35c7f7
PJDong <1115957667@qq.com>
2022-08-22 10:50:51 +0800

    fix bugs in drived mutable

--- mmrazor/models/mutables/mutable_channel/one_shot_mutable_channel.py
+++ mmrazor/models/mutables/mutable_channel/one_shot_mutable_channel.py
@@ -222,6 +222,7 @@ class OneShotMutableChannel(MutableChannel[int, Dict]):
 
         from ..mutable_value import OneShotMutableValue
         if isinstance(other, OneShotMutableValue):
-            return DerivedMutable()
+            ratio = other.current_choice
+            return self.derive_divide_mutable(ratio)
 
         raise TypeError(f'Unsupported type {type(other)} for div!')
